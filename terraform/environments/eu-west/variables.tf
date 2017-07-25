ede38b857b2d6ec141407b8c66699e062df9675c
adriagalin <adriagalin@gmail.com>
2017-07-25 17:43:48 +0200

    add refactored network module

--- terraform/environments/eu-west/variables.tf
+++ terraform/environments/eu-west/variables.tf
@@ -1,1 +1,9 @@
-variable "name" { default = "ecs-demo" }
+variable "cluster_name" {
+  type = "string"
+  description = "Name of the cluster"
+}
+
+variable "cluster_id" {
+  type = "string"
+  description = "Id of the cluster"
+}
